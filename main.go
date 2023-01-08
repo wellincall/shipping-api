355e23919ce17d61b5be3ddf17a36d6358b62e63
Wellington Calligaris <wcalligaris@gmail.com>
2023-01-08 10:54:28 +0100

    Exposing euMembers in api

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"os"
+
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getEuMembers() map[string]bool {
+	membersFile, err := os.Open("euMembers.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileInBytes, _ := ioutil.ReadAll(membersFile)
+	membersFile.Close()
+	var membersMap map[string]bool
+	json.Unmarshal([]byte(fileInBytes), &membersMap)
+	return membersMap
+}
 
 func main() {
 	r := gin.Default()
+	euMembers := getEuMembers()
 
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+		c.JSON(200, euMembers)
 	})
 
 	r.Run()
